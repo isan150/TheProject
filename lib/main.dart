9b3770e3b87a21ed4d23743a565745ec5afe8742
Ethan Jacalan <rushjet2k12@gmail.com>
2024-11-13 13:45:13 -0500

    Quick Benchmark
    Worked on my laptop for a bit, gonna save this here so I can continue on my PC when I get home.

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Course Project',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'THE HOME PAGE'),
+      );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState( => _MyHomePageState();)
+}
