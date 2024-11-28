746d7202f2e42644c44b8baf41d8a3f57533c431
Noha George Philips <nohangp@gmail.com>
2024-11-28 01:44:39 -0500

    edited main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,9 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:hellrun/game.dart';
 
-// Define the Flame game class
 void main() {
   runApp(const MyApp());
-}
-
-class FloppyBirdGame extends FlameGame {
-  @override
-  Future<void> onLoad() async {
-    // Add game setup logic here
-  }
 }
 
 class MyApp extends StatelessWidget {
@@ -20,53 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Floppy Bird',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: const MyHomePage(title: 'Floppy Bird Game'),
+      home: GameWidget(game: Moonrun()),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  late final FlameGame game;
-
-  @override
-  void initState() {
-    super.initState();
-
-    // Initialize the game
-    game = FloppyBirdGame();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Column(children: [
-          Expanded(
-            flex: 2,
-            child: Container(
-              color: Colors.blue,
-            ),
-          ),
-          Expanded(
-              child: Container(
-            color: Colors.green,
-          ))
-        ]));
   }
 }
--- lib/screens/main_menu.dart
+++ lib/screens/main_menu.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class MainMenu extends StatelessWidget {
-  const MainMenu({Key? key}) : super(key: key);
+  const MainMenu({super.key});
 
   @override
   Widget build(BuildContext context) {
