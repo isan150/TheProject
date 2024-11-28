d9321f93ed9f412acef2d7cd78a6ef8a1b452c98
Noha George Philips <nohangp@gmail.com>
2024-11-28 01:51:20 -0500

    added the main code for game.dart

---
+++ lib/game/game.dart
@@ -0,0 +1,90 @@
+import 'dart:async';
+import 'package:flame/events.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'components/astronaut.dart';
+import 'components/background.dart';
+import 'components/rod.dart';
+import 'components/rodmanager.dart';
+import 'components/score.dart';
+import 'constants.dart';
+
+import 'components/floor.dart';
+
+
+class Moonrun extends FlameGame with TapDetector, HasCollisionDetection{
+
+  late Astronaut astronaut;
+  late Background background; 
+  late Floor floor; 
+  late Rodmanager rodmanager;
+  late Scoretext scoretext; 
+
+@override
+  FutureOr<void> onLoad() {
+  
+  
+background = Background(size);
+add(background);
+
+floor = Floor();
+add(floor);
+
+rodmanager = Rodmanager(); 
+add(rodmanager);
+
+astronaut = Astronaut();
+add(astronaut);
+
+scoretext = Scoretext();
+add(scoretext);
+}
+
+
+
+  @override
+  void onTap() {
+    astronaut.leap();
+  }
+
+int score = 0; 
+void updateScore() {
+score += 1;
+}
+
+
+bool isGG = false;
+
+void gameOver() {
+  if (isGG) return;
+
+  isGG = true;
+  pauseEngine();
+
+showDialog(context: buildContext!, 
+builder: (context) => AlertDialog(
+  title: const Text("GG"),
+  content: Text("HIGH SCORE: $score"),
+  actions: [
+    TextButton(onPressed: (){
+      Navigator.pop(context);
+      resetGame();
+    }, child: const Text("restart"),)
+  ],),);
+
+
+}
+
+void resetGame() {
+  astronaut.position = Vector2(astroStartX, astroStartY);
+  astronaut.velocity = 0;
+  score = 0;
+  isGG = false;
+  children.removeWhere((child) => child is Rod);
+  //children.whereType<Rod>().forEach(action: (Rod rod) => rod.removeFromParent());
+  resumeEngine();
+  
+}
+
+
+}
