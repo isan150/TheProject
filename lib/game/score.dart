21c6ad1fc20c3d44a53d133a3b6d5e15e98a5832
Noha George Philips <nohangp@gmail.com>
2024-11-28 01:41:17 -0500

    edited score a bit and got rid of file that we dont need

--- lib/game/score.dart
+++ lib/game/score.dart
@@ -1,33 +1,30 @@
 import 'dart:async';
-
 import 'package:flame/components.dart';
-import 'package:flame/text.dart';
 import 'package:flutter/material.dart';
-import 'floppy_bird_game.dart';
+import '../game.dart';
 
-class ScoreText extends TextComponent with HasGameRef<FloppyBirdGame>{
-  // init
-  ScoreText() : super(
-      text: '0',
-      textRenderer: TextPaint(
-        style: TextStyle( color: Colors.white, fontSize: 50),
-      ),
-  );
+class Scoretext extends TextComponent with HasGameRef<Moonrun>{
 
-  //load
-  @override
-  FutureOr<void> onLoad() {
-    //Setting position to lower middle
-    position = Vector2((gameRef.size.x - size.x) / 2, gameRef.size.y - size.y - 50,);
+  Scoretext() : super(text: '0',
+  textRenderer: TextPaint(
+    style: const TextStyle(
+    color: Colors.purple,
+    fontSize: 48,
+  ),),);
+
+@override
+FutureOr<void> onLoad(){
+  position = Vector2((gameRef.size.x - size.x)/2, gameRef.size.y - size.y - 50,);
+}
+
+@override
+void update(double dt){
+  final ntext = gameRef.score.toString();
+  if (text != ntext){
+    text = ntext;
   }
+}
 
-  // Update
-  @override
-  void update (double dt){
-    final newText = gameRef.score.toString();
-    if (text != newText) {
-      text = newText;
-    }
-  }
 
 }
+
