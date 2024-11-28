b3bf717c0f0065ae6376d58eee321898cb485994
Noha George Philips <nohangp@gmail.com>
2024-11-28 01:47:50 -0500

    added game floor

---
+++ lib/game/floor.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+import 'package:hellrun/constants.dart';
+
+import '../game.dart';
+
+class Floor extends SpriteComponent with HasGameRef<Moonrun>, CollisionCallbacks {
+
+Floor() : super();
+
+@override
+  FutureOr<void> onLoad() async {
+  size = Vector2(2 * gameRef.size.x, floorHeight);
+  position = Vector2(0,gameRef.size.y - floorHeight);
+
+
+  sprite = await Sprite.load('floor.png');
+
+
+  add(RectangleHitbox());
+}
+
+@override
+  void update(double dt) { 
+    position.x -= floorSpeed * dt;
+  
+if (position.x + size.x / 2 <= 0)
+{
+  position.x = 0;
+}
+}}
