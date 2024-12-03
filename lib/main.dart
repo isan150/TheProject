d78723ce87b92e71a96a02c8e06a447b2e50bfdb
Ethan Jacalan <rushjet2k12@gmail.com>
2024-12-02 22:28:42 -0500

    Attempt at Notifications
    I have the setup for notifications, but they're commented out cuz I'm having errors with them and I'm not sure how to fix them yet..

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,120 @@
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
-import 'package:flappy_bird_clone/screens/main_menu.dart';
-import 'package:flappy_bird_clone/screens/settings.dart';
-import 'package:flappy_bird_clone/game/game.dart';
+import 'package:flutter/services.dart';
+import 'game/game.dart';
+import 'screens/main_menu.dart';
+import 'screens/settings.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-void main() {
-  runApp(const MyApp());
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  
+  //runApp(const MyApp());
+  
+  final game = Moonrun();
+  runApp(
+    GameWidget(
+      game: game,
+      initialActiveOverlays: const [MainMenu.id],
+      overlayBuilderMap: {
+        'mainMenu': (context, _) => MainMenu(game: game),
+      }
+    )
+  );
 }
 
+// Setup for notifications below
+/*
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: NotificationsScreen()
+      );
+  }
+}
+
+class NotificationsScreen extends StatefulWidget {
+  @override
+  _NotificationScreenState createState() => _NotificationScreenState();
+}
+
+
+class _NotificationScreenState extends State<NotificationsScreen> {
+  FlutterLocalNotificationsplugin flutterLocalNotificationsPlugin = 
+  FlutterLocalNotificationsPlugin();
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeNotifications();
+  }
+}
+
+ // Initialize notifications
+  void _initializeNotifications() async {
+    var androidInitialization = AndroidInitializationSettings('app_icon');
+    var initializationSettings = InitializationSettings(
+      android: androidInitialization,
+    );
+    
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  }
+
+  // Schedule daily notification
+  void _scheduleDailyNotification() async {
+    var time = Time(13, 30, 0); // Notifications at 1:30pm daily
+
+    var androidDetails = AndroidNotificationDetails(
+      'daily_notification_channel',
+      'Daily Notifications',
+      channelDescription: 'This channel is used for daily notifications.',
+      importance: Importance.high,
+      priority: Priority.high,
+    );
+
+    var notificationDetails = NotificationDetails(
+      android: androidDetails
+    );
+
+    await flutterLocalNotificationsPlugin.showDailyAtTime(
+      0, // notification id
+      'Come and play Moon Jump!',
+      'Try for a new high score today!',
+      time, // the time to trigger the notification
+      notificationDetails,
+    );
+  }
+
+ @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Notification to Play')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: _scheduleDailyNotification,
+          child: Text('Schedule Daily Notification'),
+        ),
+      ),
+    );
+  }
+
+*/
+
+/*
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final Moonrun game = Moonrun();
-
     return MaterialApp(
-      title: 'Moon Jump',
-      theme: ThemeData(primarySwatch: Colors.pink),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => MainMenu(game: game), // Register "/" for the main menu
-        '/settings': (context) => Settings(game: game), // Register settings
-      },
+      debugShowCheckedModeBanner: false,
+      home: GameWidget(game: Moonrun(), overlayBuilderMap: {
+        'mainMenu': (context, _) => MainMenu(),
+      }),
     );
   }
 }
+*/
